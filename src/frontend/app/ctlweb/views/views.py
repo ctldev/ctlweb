a9fac9b4a7a9cdba1ca583dcb99e66cbeb15d8d7
Johann Hong <johann.hong@gmail.com>
2012-12-21 15:38:36 +0100

    erste views.py einträge

--- src/frontend/app/ctlweb/views/views.py
+++ src/frontend/app/ctlweb/views/views.py
@@ -1,1 +1,9 @@
 # Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render_to_response
+from django.template import Context, loader
+
+
+def index(request): 
+    context = Context(get_dict_response(request))
+    return render_to_response("index.html",context_instance=context)
