4d78fe197ab6d6796f437d0879f150fc74de23cb
2haustier <Stephan.Sobol@web.de>
2012-12-21 12:42:03 +0100

    datenbank erstellt

--- src/frontend/app/ctlweb/models/models.py
+++ src/frontend/app/ctlweb/models/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class webserver(models.Model):
+    ip = models.IPAddressField(null="True")
+    domain = models.CharField(max_length=100 ,null="True")
+    port = models.IntegerField()
+    class Meta:
+        unique_together = ('ip', 'domain')
+        
+class cluster(models.Model):
+    ip = models.IPAddressField(null="True")
+    domain = models.CharField(max_length=100, null="True")
+    port = models.IntegerField()
+    key = models.TextField(null="True")
+    class Meta:
+        unique_together = ('ip', 'domain')
+        
+class userkeys(models.Model):
+    user = models.OneToOneField(User)
+    key = models.TestField()
+    class Meta:
+        unique_together = ('user', 'key')
+        
+class components(models.Model):
+    name = models.CharField(max_length=100, unique="True")
+    pfad = models.TextField()
+    homeserver = models.ManyToManyField(webserver)
+    homecluster = models.ManyToManyField(cluster)
+    programmer = models.EmailField()
+    discription = models.TextField()
+    
