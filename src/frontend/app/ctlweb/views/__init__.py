0353feb434b289b33ca7f3cde5d0acc635465c01
Johann Hong <johann.hong@gmail.com>
2013-01-11 16:00:55 +0100

    init py

---
+++ src/frontend/app/ctlweb/views/__init__.py
@@ -0,0 +1,5 @@
+from views import *
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render_to_response
+from django.template import Context, loader
+from django.template import RequestContext, Template 
