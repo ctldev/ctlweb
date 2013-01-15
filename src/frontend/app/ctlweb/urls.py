c376c41472467d2ee1cc2b9d613b9ce7cb340d04
Philipp Offensand <philippoffensand@gmx.de>
2013-01-15 15:56:34 +0100

    Hyperlinks über den django-{%url%}-Tag gelöst

--- src/frontend/app/ctlweb/urls.py
+++ src/frontend/app/ctlweb/urls.py
@@ -3,8 +3,13 @@ from django.views.generic.simple import direct_to_template
 from app.ctlweb.views import *
 
 urlpatterns = patterns('app.ctlweb.views',
-        url(r'^$', 'index'), 
-        url(r'^search/$', 'search') 
+        url(r'^$', 'index', name='index'), 
+        url(r'^search/$', 'search', name='search'),
+        url(r'^components/$', 'index', name='components'),
+        url(r'^administration/$', 'index', name='administration'),
+        url(r'^administration/easy/$', 'index', name='administration_easy'),
+        url(r'^administration/advanced/$', 'index', name='administration_advanced'),
+        url(r'^impressum/$', 'index', name='impressum'),
 )
 
 
