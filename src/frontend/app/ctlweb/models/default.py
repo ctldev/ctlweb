be2ccce248f3fad1a5c2f9046dcb85159275e56f
2haustier <Stephan.Sobol@web.de>
2013-01-15 11:30:31 +0100

    erste Anpassung der models.py für die forms

--- src/frontend/app/ctlweb/models/default.py
+++ src/frontend/app/ctlweb/models/default.py
@@ -1,36 +1,53 @@
+# vim: set fileencoding=utf-8
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class webserver(models.Model):
-    ip = models.IPAddressField(null="True")
-    domain = models.CharField(max_length=100 ,null="True")
-    port = models.IntegerField()
+    name = models.CharField(_("Name"), max_length=255)
+    ip = models.IPAddressField(_("IP"), null="True")
+    domain = models.CharField(_("Domain"), max_length=100 ,null="True")
+    port = models.IntegerField(_("Port"))
     class Meta:
         unique_together = ('ip', 'domain')
         app_label = 'ctlweb'
-        
+        verbose_name = _("Webserver")
+        verbose_name_plural = _("Webserver")
+                
 class cluster(models.Model):
-    ip = models.IPAddressField(null="True")
-    domain = models.CharField(max_length=100, null="True")
-    port = models.IntegerField()
-    key = models.TextField(null="True")
+    ip = models.IPAddressField(_("IP"), null="True")
+    domain = models.CharField(_("Domain"), max_length=100, null="True")
+    port = models.IntegerField(_("Port"))
+    key = models.TextField(_(u"Schlüssel"), null="True")
     class Meta:
         unique_together = ('ip', 'domain')
         app_label = 'ctlweb'
+        verbose_name = _("Cluster")
+        verbose_name_plural = _("Cluster")
         
 class userkeys(models.Model):
-    user = models.OneToOneField(User)
-    key = models.TextField()
+    user = models.OneToOneField(User, verbose_name=_("User"))
+    key = models.TextField(_(u"Schlüssel"))
     class Meta:
         unique_together = ('user', 'key')
         app_label = 'ctlweb'
+        verbose_name = _(u"Benutzerschlüssel")
+        verbose_name_plural = _(u"Benutzerschlüssel")
         
 class components(models.Model):
-    name = models.CharField(max_length=100, unique="True")
-    pfad = models.TextField()
-    homeserver = models.ManyToManyField(webserver)
-    homecluster = models.ManyToManyField(cluster)
-    programmer = models.EmailField()
-    discription = models.TextField()
+    name = models.CharField(_("Name"), max_length=100, unique="True")
+    path = models.TextField(_("Pfad"))
+    homeserver = models.ManyToManyField(webserver, 
+                                        verbose_name=_("Ursprungsserver"))
+    homecluster = models.ManyToManyField(cluster,
+                                         verbose_name=_("Ursprungscluster"))
+    programmer = models.EmailField(_("Programmierer"))
+    brief_description = models.CharField(_("Kurzbeschreibung"), max_length=255)
+    description = models.TextField(_("Beschreibung"))
+    date = models.DateField(_("Datum"), auto_now_add=True)
+    is_active = models.BooleanField(_("Freigeschaltet"))
+    code = models.TextField(_("Implementierungscode"))
     class Meta:
         app_label = 'ctlweb'
+        verbose_name = _("Component")
+        verbose_name_plural = _("Components")        
