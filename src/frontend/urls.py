9efd4211680ea0c912c385cb7355b97a965b3f22
Johann Hong <johann.hong@gmail.com>
2013-01-15 07:41:38 +0100

    Base.html geupdatet

--- src/frontend/urls.py
+++ src/frontend/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+                 {'document_root': settings.MEDIA_ROOT}),
     url(r'^', include('app.ctlweb.urls')), 
     # Examples:
     # url(r'^$', 'frontend.views.home', name='home'),
