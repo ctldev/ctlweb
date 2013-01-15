4d11e0007f53360abe35a24e3093c10e83d01b0e
sukram <celebrir@gmx.de>
2013-01-15 13:38:58 +0100

    Suchdateien gegitaddet

--- src/frontend/app/ctlweb/views/search.py
+++ src/frontend/app/ctlweb/views/search.py
@@ -1,6 +1,9 @@
-from views import *
-from search import *
+# Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from django.template import Context, loader
 from django.template import RequestContext, Template 
+
+
+def search(request): 
+    return render_to_response("search.html", context_instance=RequestContext(request))
