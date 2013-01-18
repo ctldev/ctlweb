d6c51dbeb9da51a4815e30d2559c61dfb6b6fb97
Johann Hong <johann.hong@gmail.com>
2013-01-18 13:50:14 +0100

    Optische Anpassung im CSS; Erste Template/View für Login

--- src/frontend/app/ctlweb/views/login.py
+++ src/frontend/app/ctlweb/views/login.py
@@ -1,7 +1,8 @@
-from views import *
-from search import *
-from login import *
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from django.template import Context, loader
 from django.template import RequestContext, Template 
+
+def login(request):
+    
+    return render_to_response("login.html", context_instance=RequestContext(request))
