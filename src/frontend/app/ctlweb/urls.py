6e2f5e7743d05018fb0bf09211374dba6e7355bf
Johann Hong <johann.hong@gmail.com>
2013-01-22 13:42:50 +0100

    Registrieierung eingebunden

--- src/frontend/app/ctlweb/urls.py
+++ src/frontend/app/ctlweb/urls.py
@@ -2,15 +2,18 @@ from django.conf.urls.defaults import patterns, url
 from django.views.generic.simple import direct_to_template
 from app.ctlweb.views import *
 
+
 urlpatterns = patterns('app.ctlweb.views',
         url(r'^$', 'index', name='index'), 
-        url(r'^login/$', 'login', name='login'), 
+        #url(r'^login/$', 'login', name='login'), 
         url(r'^search/$', 'search', name='search'),
         url(r'^components/$', 'index', name='components'),
         url(r'^administration/$', 'index', name='administration'),
         url(r'^administration/easy/$', 'index', name='administration_easy'),
         url(r'^administration/advanced/$', 'index', name='administration_advanced'),
         url(r'^impressum/$', 'index', name='impressum'),
+        #url(r'^accounts/login/$', None, name='login'), 
+        #url(r'^accounts/register/$', None, name='register')
 )
 
 
--- src/frontend/urls.py
+++ src/frontend/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls.defaults import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
@@ -17,5 +17,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('registration.backends.default.urls')), 
 )
