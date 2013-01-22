abda113f0bba8c8efa0f6490da59b5677623c2ed
2haustier <Stephan.Sobol@web.de>
2013-01-22 13:47:37 +0100

    componentendetails(1) + datenbankänderungen

--- src/frontend/app/ctlweb/models/default.py
+++ src/frontend/app/ctlweb/models/default.py
@@ -33,21 +33,40 @@ class userkeys(models.Model):
         app_label = 'ctlweb'
         verbose_name = _(u"Benutzerschlüssel")
         verbose_name_plural = _(u"Benutzerschlüssel")
-        
+	permissions = (
+		("can_request_key", _(u"Darf sich einen Schlüssel anfordern")),
+		("can_activate_user", _("Darf User freischalten")),
+		("can_ban_user", _("Darf User bannen")))        
+
 class components(models.Model):
     name = models.CharField(_("Name"), max_length=100, unique="True")
-    path = models.TextField(_("Pfad"))
     homeserver = models.ManyToManyField(webserver, 
                                         verbose_name=_("Ursprungsserver"))
     homecluster = models.ManyToManyField(cluster,
+                                         through='components_cluster',
                                          verbose_name=_("Ursprungscluster"))
     programmer = models.EmailField(_("Programmierer"))
     brief_description = models.CharField(_("Kurzbeschreibung"), max_length=255)
     description = models.TextField(_("Beschreibung"))
     date = models.DateField(_("Datum"), auto_now_add=True)
     is_active = models.BooleanField(_("Freigeschaltet"))
-    code = models.TextField(_("Implementierungscode"))
     class Meta:
         app_label = 'ctlweb'
         verbose_name = _("Component")
         verbose_name_plural = _("Components")        
+    permissions = (
+		    ("can_see_description", _("Darf Beschreibungen lesen")),
+		    ("can_see_homecluster", _("Darf die Ursprungscluster sehen")),
+     		("can_see_homeserver", _("Darf die Ursprungsserver sehen")))
+
+class components_cluster(models.Model):
+    component = models.ForeignKey(components)
+    cluster = models.ForeignKey(cluster)
+    path = models.TextField(_("Pfad"))
+    code = models.TextField(_("Implementierungscode"))
+    class Meta:
+        app_label = 'ctlweb'
+    permissons = (
+            ("can_see_path", _("Darf den Pfad sehen")),
+            ("can_see_code", _("Darf den Implementierungscode sehen"))),
+
--- src/frontend/app/ctlweb/views/__init__.py
+++ src/frontend/app/ctlweb/views/__init__.py
@@ -1,6 +1,7 @@
 from views import *
 from search import *
 from login import *
+from component import *
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from django.template import Context, loader
