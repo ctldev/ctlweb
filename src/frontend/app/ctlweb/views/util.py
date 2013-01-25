24dc9391f65d3a575231308284d9ceacea71d235
sukram <celebrir@gmx.de>
2013-01-25 16:29:56 +0100

    Allgemeine Suche in views/util.py

--- src/frontend/app/ctlweb/views/util.py
+++ src/frontend/app/ctlweb/views/util.py
@@ -1,1 +1,14 @@
 #vim: set fileencoding=utf-8
+
+from django.db.models import Q
+from app.models import Components
+
+def __searchset(searchtext):
+    return (Q(name__icontains = searchtext)|
+            Q(programmer__icontains = searchtext)|
+            Q(description__icontains = searchtext)|
+            Q(homeserver__icontains = searchtext)|
+            Q(date__icontains = searchtext))
+
+def search(searchtext):
+    return Components.objects.filter(__searchset(searchtext))
