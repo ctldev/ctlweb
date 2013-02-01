78cd4899e9df1d55962ead64e3aa502550274bfc
sukram <celebrir@gmx.de>
2013-02-01 13:12:15 +0100

    View der einfachen Suche integriert

--- src/frontend/app/ctlweb/views/util.py
+++ src/frontend/app/ctlweb/views/util.py
@@ -1,14 +1,15 @@
 #vim: set fileencoding=utf-8
 
 from django.db.models import Q
-from app.models import Components
+from ctlweb.models import Components
 
 def __searchset(searchtext):
     return (Q(name__icontains = searchtext)|
-            Q(programmer__icontains = searchtext)|
+            Q(programmer__iexact = searchtext)|
             Q(description__icontains = searchtext)|
-            Q(homeserver__icontains = searchtext)|
+            Q(homeserver__name__icontains = searchtext)|
+            Q(homeserver__ip__icontains = searchtext)|
             Q(date__icontains = searchtext))
 
-def search(searchtext):
+def simple_search(searchtext):
     return Components.objects.filter(__searchset(searchtext))
