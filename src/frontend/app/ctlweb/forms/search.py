c58dc1adba479a31ddf0924a352072b440085a54
Philipp Offensand <philippoffensand@gmx.de>
2013-02-01 13:40:37 +0100

    Internationalisierung fortgeführt

--- src/frontend/app/ctlweb/forms/search.py
+++ src/frontend/app/ctlweb/forms/search.py
@@ -1,26 +1,29 @@
 #vim: set fileencoding=utf-8
 from django import forms
+from django.utils.translation import ugettext as _
 
 SEARCH_CATEGORY_CHOICES = (
-        ('all', 'Alle Kategorien'),
-        ('name', 'Name'),
-        ('author', 'Autor'),
-        ('keywords', 'Schusselwort'),
-        ('homeserver', 'Server'),
-        ('date', 'Datum'),)
+        ('all', _('Alle Kategorien')),
+        ('name', _('Name')),
+        ('author', _('Autor')),
+        ('keywords', _(u'Schlüsselwort')),
+        ('homeserver', _('Server')),
+        ('date', _('Datum')),)
 
 class SearchForm(forms.Form):
-    Suchtext = forms.CharField()
-    Kategorie = forms.ChoiceField(choices = SEARCH_CATEGORY_CHOICES)
+    Suchtext = forms.CharField(label=_("Suchtext"))
+    Kategorie = forms.ChoiceField(choices = SEARCH_CATEGORY_CHOICES,
+        label=_("Kategorie"))
 
 class AddSearchForm(forms.Form):
     LOGIC_CHOICES = (
             ('and', 'und'),
             ('and not', 'und nicht'),
             ('or', 'oder'),)
-    Bindung = forms.ChoiceField(choices = LOGIC_CHOICES)
-    Suchtext = forms.CharField()
-    Kategorie = forms.ChoiceField(choices = SEARCH_CATEGORY_CHOICES)
+    Bindung = forms.ChoiceField(choices = LOGIC_CHOICES, label=_("Bindung"))
+    Suchtext = forms.CharField(label=_("Suchtext"))
+    Kategorie = forms.ChoiceField(choices = SEARCH_CATEGORY_CHOICES,
+        label=_("Kategorie"))
 
     class Media:
         js = ('js/dynamic-formset.js',)
