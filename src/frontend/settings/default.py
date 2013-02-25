f1d71feec63206ef8266a94082efd8f857f5123c
Philipp Offensand <philippoffensand@gmx.de>
2013-02-25 15:50:49 +0100

    Import manifest fortgeführt, manage.py demo_data
    das Modul zum erstellen der Demo-Daten ist aus dem Modul tests verschwunden und
    nun über den Befehl 'python manage.py demo_data' verfügbar.

--- src/frontend/settings/default.py
+++ src/frontend/settings/default.py
@@ -11,9 +11,6 @@ except ImportError:
     from random import choice
     USE_CHOICE = True
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
 DJANGO_ROOT = dirname(dirname(abspath(__file__)))
 
 SITE_NAME = basename(DJANGO_ROOT)
@@ -31,30 +28,6 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ctldata',                      # Or path to database file if using sqlite3.
-        'USER': 'superuser',                      # Not used with sqlite3.
-        'PASSWORD': 'ctlweb2012',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'Europe/Berlin'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'de-de'
 
 SITE_ID = 1
 
@@ -105,9 +78,6 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-#SECRET_KEY = '+32v8r#app+@acf%3hd0nlp%^mm81v5&2hg+72ur%i6un)8oy-'
-
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
@@ -150,37 +120,6 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
-
-ACCOUNT_ACTIVATION_DAYS = 7
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-DEFAULT_FROM_EMAIL = 'teamprojektctlweb@gmail.com'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'teamprojektctlweb@gmail.com'
-EMAIL_HOST_PASSWORD = 'ctlweb2012'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-
 
 try:
     SECRET_KEY = open(SECRET_FILE).read().strip()
--- src/frontend/settings/dev.py
+++ src/frontend/settings/dev.py
@@ -4,6 +4,21 @@
 from default import *
 from os.path import join, normpath
 
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# On Unix systems, a value of None will cause Django to use the same 
+# timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'Europe/Berlin'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'de-de'
+
+SITE_DOMAIN = 'localhost'
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -18,3 +33,34 @@ DATABASES = {
         }
     }
 
+ACCOUNT_ACTIVATION_DAYS = 7
+
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
+
+DEFAULT_FROM_EMAIL = 'teamprojektctlweb@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'teamprojektctlweb@gmail.com'
+EMAIL_HOST_PASSWORD = 'ctlweb2012'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
