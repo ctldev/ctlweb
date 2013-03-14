cb7695cad358091af1c6f41e48515bac0e8d6f7f
sukram <celebrir@gmx.de>
2013-03-14 15:11:03 +0100

    Erweiterte Suche letzte Bugfix und Kommentare

--- src/frontend/app/ctlweb/views/search.py
+++ src/frontend/app/ctlweb/views/search.py
@@ -13,52 +13,74 @@ def search(request):
     """Grundfunktion der Suche. Wenn noch keine Sucheingabe vorhanden ist, wird
     das Suchformular geöffnet. Wenn eine Sucheingabe vorhanden ist, wird diese
     verarbeitet und an die Listenfunktion weitergegeben."""
-    #Erstellung der Formsets aus dem Import der forms
+#Erstellung der Formsets aus dem Import der forms
     SearchFormset = formset_factory(SearchForm)
     AddSearchFormset = formset_factory(AddSearchForm)
-    #Abfrage ob schon eine Sucheingabe vorhanden ist.
+#Abfrage ob schon eine Sucheingabe vorhanden ist.
     if request.method == 'POST':
-        #Auslesen der Sucheingaben und initialiseren der Suchergebnisvariablen
+#Auslesen der Sucheingaben und initialiseren der Suchergebnisvariablen
         filled_areaform = SearchAreaForm(request.POST, prefix='areaform')
         filled_baseform = SearchFormset(request.POST, prefix='baseform')
         filled_addform = AddSearchFormset(request.POST, prefix='addform')
-        searched_comps = Components.objects.all()
-        searched_interfaces = Interfaces.objects.all()
+#Start der Auswertung des Forms zur Bestimmung des Suchegebiet
         if filled_areaform.is_valid():
             searcharea = filled_areaform.cleaned_data['area']
+#Initialisierung der Suchvariablen je nachdem in welchem Gebiet gesucht wird.
+            if searcharea == 'all' or searcharea == 'components':
+#Wenn in dem Gebiet gesucht wird, alle Komponenten in die Variable
+                searched_comps = Components.objects.all()
+            else :
+#Wenn in dem Gebiet nicht gesucht wird, keine Components in die Variable.
+                searched_comps = Components.objects.none()
+            if searcharea == 'all' or searcharea == 'interfaces':
+#Das Gleiche für Interfaces
+                searched_interfaces = Interfaces.objects.all()
+            else :
+                searched_interfaces = Interfaces.objects.none()
 
-        #Auswertung der Forms mit Unterfunktionen für Component und Interface
+#Auswertung des ersten Suchformulars. Für jede Zeile in der Form
             for form in filled_baseform:
                 if form.is_valid():
+#Auslesen der Eingaben aus dem Form. bind=first, da erster Suchbegriff
                     searchtext = form.cleaned_data['searchtext']
                     category = form.cleaned_data['category']
                     bind = "first"
-                    if searcharea == "all" or searcharea == "interfaces":
+#Aufruf der Interface- bzw. Componentsuche. Ignoriert leere Suchbegriffe
+                    if searcharea == "all" or searcharea == "interfaces" and \
+                            searchtext != "":
                         searched_interfaces = search_interfaces(searchtext, 
                                 bind, category, searched_interfaces)
-                    if searcharea == "all" or searcharea == "components":
+                    if searcharea == "all" or searcharea == "components" and \
+                            searchtext != "":
                         searched_comps = search_comps(searchtext, bind, 
                                 category, searched_comps)
+#Basis selbe Auswertung, wie für erste Suchformular für weitere Suchformulare
             for form in filled_addform:
                 if form.is_valid():
                     searchtext = form.cleaned_data['searchtext']
                     category = form.cleaned_data['category']
+#bind diesmal aus dem Formular gelesen, da unterschiedliche Möglichkeiten.
                     bind = form.cleaned_data['bind']
-                    if searcharea == "all" or searcharea == "interfaces":
+                    if searcharea == "all" or searcharea == "interfaces" and \
+                            searchtext != "":
                         searched_interfaces = search_interfaces(searchtext, 
                                 bind, category, searched_interfaces)
-                    if searcharea == "all" or searcharea == "components":
+                    if searcharea == "all" or searcharea == "components" and \
+                            searchtext != "":
                         searched_comps = search_comps(searchtext, bind, 
                                 category, searched_comps)
+#Entfernen der inaktiven Components aus dem Suchergebnis
             searched_comps = searched_comps.exclude(is_active__icontains =
             "False")
+#Bestimmen der Interfaces, die nicht gefunden wurden, aber zu gefundenen
+#aber zu gefundenen Komponenten gehören
             indirect_interfaces = Interfaces.objects.none()
-        #Weiterleiten der nun gefüllten Ergebnisvariablen an die Listenfunktion
             for comp in searched_comps:
                 indirect_interfaces = indirect_interfaces | Interfaces.objects.all().filter(components__name__iexact = comp.name)
+            indirect_interfaces = indirect_interfaces.distinct()
             return lists(request, searched_interfaces, indirect_interfaces,
                     searched_comps, 1)
-    #Erstellen des Suchformulars falls keine Suchanfrage vorhanden ist
+#Erstellen des Suchformulars falls keine Suchanfrage vorhanden ist
     areaform = SearchAreaForm(prefix='areaform')
     baseform = SearchFormset(prefix='baseform')
     addform = AddSearchFormset(prefix='addform')
@@ -73,59 +95,85 @@ def search(request):
 
 def search_interfaces(searchtext, bind, category, searched_interfaces):
     """Grundlegende Suche der interfaces, die nacheinander für alle
-    Verbindungen Suchen startet"""
+    Verbindungen Suchen startet. Wird von search() mit den Daten des Formulares
+    aufgerufen. Der erste Suchdurchgang ist immer eine und-Suche auf alle
+    Komponente."""
+#Überprüfung welches Bind aktiv
     if bind == "and" or bind == "first":
+#Aufruf der passenden Suche
         searched_interfaces = interface_and_search(searchtext,
                 category, searched_interfaces)
+#Wenn der vorherige Bind nicht der Aktive, Überprüfung auf den Nächsten.
     elif bind == "or":
         searched_interfaces = interface_or_search(searchtext,
                 category, searched_interfaces)
+#Wenn der vorherige Bind nicht der Aktive, Überprüfung auf den Nächsten.
     elif bind == "and not":
         searched_interfaces = interface_and_not_search(searchtext,
                 category, searched_interfaces)
+#Rückgabe des modifizierten Suchergebnisses
     return searched_interfaces
 
 def interface_and_search(searchtext, category, searched_interfaces):
-    """Modifzierung des Suchergebnisses bei einer interface und Verknüpfung"""
+    """Modifzierung des Suchergebnisses bei einer interface und Verknüpfung.
+    Wird von search_interfaces aufgerufen."""
+#Überprüfen in welcher Kategorie gesucht wird.
     if category == 'name':
+#Filtern nach allen Interfaces die das Suchkriterium erfüllen.
         searched_interfaces = searched_interfaces.filter(
             name__icontains = searchtext)
     elif category == "keywords":        
         searched_interfaces = searched_interfaces.filter(
             description__icontains = searchtext)
+#Sonderfall all-Suche: Es soll ein Interface gefunden werden, dass in einer
+#Kategorie dem Suchbegriff entspricht, aber nicht zwangsläufig in jeder.
     elif category == 'all': 
+#Einzelnes Suchen nach den Interfaces die in einer Kategorie passen.
         searched_names = searched_interfaces.filter(
             name__icontains = searchtext)
         searched_keywords = searched_interfaces.filter(
             description__icontains = searchtext)
+#Zusammenfügen aller gefundenen Interfaces
         searched_interfaces = searched_names | searched_keywords
     return searched_interfaces
 
 def interface_and_not_search(searchtext, category, searched_interfaces):
-    """Modifzierung des Suchergebnisses bei einer interface und nicht Verknüpfung"""
+    """Modifzierung des Suchergebnisses bei einer interface und nicht
+    Verknüpfung. Wird von search_interfaces() aufgerufen. Hier ist die all-Suche
+    kein Sonderfall"""
+#Überprüfung welche Kategorie aktiv. 
     if category == 'name' or category == 'all':
+#Entfernen aller Interfaces auf die der Suchbegriff nicht passt.
         searched_interfaces = searched_interfaces.exclude(
             name__icontains = searchtext)
+#Da im Falle einer all-Suche alle Schleifen bearbeitet werden if statt elif
     if category == "keywords" or category == 'all':        
         searched_interfaces = searched_interfaces.exclude(
             description__icontains = searchtext)
     return searched_interfaces
 
-def interfaces_or_search(searchtext, category, searched_interfaces):
-    """Modifzierung des Suchergebnisses bei einer interface oder Verknüpfung"""
+def interface_or_search(searchtext, category, searched_interfaces):
+    """Modifzierung des Suchergebnisses bei einer interface oder Verknüpfung.
+    Wird von search_interfaces() aufgerufen. Hier ist die all-Suche keine
+    Ausnahme"""
+#Überprüfung welche Kategorie aktiv. 
     if category == 'name' or category == 'all':
-        search_query = Components.objects.filter(
+#Suche aller Interface auf die der Suchbegriff passt
+        search_query = Interfaces.objects.filter(
             name__icontains = searchtext)
+#Hinzufügen der gefundenen Ergebnisse zu den Alten
         searched_interfaces = searched_interfaces | search_query
+#Da im Falle einer all-Suche alle Schleifen bearbeitet werden if statt elif
     if category == "keywords" or category == 'all':        
-        search_query = Components.objects.filter(
-            brief_description__icontains = searchtext)
+        search_query = Interfaces.objects.filter(
+                description__icontains = searchtext)
         searched_interfaces = searched_interfaces | search_query
+#Distinct um Doppelfindungen durch Zusammenfügen zu vermeiden.
+    searched_interfaces = searched_interfaces.distinct()
     return searched_interfaces
 
 def search_comps(searchtext, bind, category, searched_comps):
-    """Grundlegende Suche der components, die nacheinander für alle
-    Verbindungen Suchen startet"""
+    """Wie search_interfaces(), allerdings für Komponenten"""
     if bind == "and" or bind == "first":
         searched_comps = and_search(searchtext, category, searched_comps)
     if bind == "or":
@@ -135,7 +183,7 @@ def search_comps(searchtext, bind, category, searched_comps):
     return searched_comps
 
 def and_search(searchtext, category, searched_comps):
-    """Modifzierung des Suchergebnisses bei einer component und Verknüpfung"""
+    """Wie interface_and_search(), allerdings für Komponenten"""
     if category == 'name' or category == 'all':
         searched_comps = searched_comps.filter(
             name__icontains = searchtext)
@@ -164,7 +212,7 @@ def and_search(searchtext, category, searched_comps):
     return searched_comps
         
 def and_not_search(searchtext, category, searched_comps):
-    """Modifzierung des Suchergebnisses bei einer component und nicht Verknüpfung"""
+    """Wie interface_and_not_search(), allerdings für Komponenten"""
     if category == 'name' or category == 'all':
         searched_comps = searched_comps.exclude(
             name__icontains = searchtext)
@@ -182,7 +230,7 @@ def and_not_search(searchtext, category, searched_comps):
     return searched_comps
 
 def or_search(searchtext, category, searched_comps):
-    """Modifzierung des Suchergebnisses bei einer component und Verknüpfung"""
+    """Wie interface_or_search(), allerdings für Komponenten"""
     if category == 'name' or category == 'all':
         search_query = Components.objects.filter(
             name__icontains = searchtext)
@@ -202,8 +250,10 @@ def or_search(searchtext, category, searched_comps):
         search_query = Components.objects.filter(
             name__icontains = searchtext)
         searched_comps = searched_comps | search_query
-        searched_comps = searched_comps.filter(
+        search_query = Components.objects.filter(
             date__icontains = searchtext)
+        searched_comps = searched_comps | search_query
+    searched_comps = searched_comps.distinct()
     return searched_comps
 
 
