2dea19403802c5ef387a56c066caa7a615f2be35
Theodor van Nahl <theo_dev@van-nahl.org>
2013-09-10 00:16:51 +0200

    ctl-genmanifest has been rewritten as ctl-build-component.
    ctl-genmanifest was written with optparse, was hard to maintain and had a
    problem with component documentation files. Due to that it was rewritten as
    ctl-build-component which has a new Feature: ctl-build-component is independet
    from the ctlweb backend libraries. It only depends for the sake of easy
    deployment on the standard python3.2 libraries.

--- lib/backend/util/util.py
+++ lib/backend/util/util.py
@@ -5,15 +5,3 @@ from database import Database
 from database import User
 from database import Web
 from .settings import DEFAULT_CONFIG
- 
-def hash_file(file, block_size=512):
-    import hashlib
-    import base64
-    md5 = hashlib.md5()
-    with open(file,'rb') as f:
-        while True:
-            data = f.read(block_size)
-            if not data:
-                break
-            md5.update(data)
-    return base64.b64encode(md5.digest()).decode('utf8')
