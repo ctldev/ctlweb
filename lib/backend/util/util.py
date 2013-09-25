b34cfb094a4eed9192f2f96160b685714aeb2623
Theodor van Nahl <theo_dev@van-nahl.org>
2013-09-26 01:14:55 +0200

    Bugfixes in backend for backend/frontend-interface. And...
    some cool Logging features.

--- lib/backend/util/util.py
+++ lib/backend/util/util.py
@@ -9,17 +9,22 @@ def push(url, components=None, component_logs=None):
 
     files = {}
     data = {}
+    info_data = {'manifest': 'no manifest',
+                 'hash': 'no hash'}
 
     if components:
         comp = components.pop()
         manifest_file = comp._component_file
         files['manifest'] = open(manifest_file, 'rb')
+        info_data['manifest'] = manifest_file
     if component_logs:
         complog = component_logs.pop()
-        data['exe_hash'] = complog.exe_hash
+        data['exe_hash'] = complog.hash
+        info_data['hash'] = complog.hash
     if files or data:
+        Log.info('Uploading {hash} and {manifest}'.format(**info_data))
         r = requests.post(url, data=data, files=files)
         if r.status_code != requests.codes.ok:
-            Log.critical('Error %s occured while upload' % r.status_code)
+            Log.warning('Error %s occured while upload' % r.status_code)
     if components or component_logs:
         push(url, components, component_logs)
