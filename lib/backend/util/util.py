341ac6c3d2bb353dc292e8403991484860702694
Theodor van Nahl <theo_dev@van-nahl.org>
2013-09-25 10:51:48 +0200

    added basic logging for ctlweb-backend

--- lib/backend/util/util.py
+++ lib/backend/util/util.py
@@ -1,4 +1,4 @@
-import Log
+from .log import Log
 
 
 def push(url, components=None, component_logs=None):
@@ -7,17 +7,18 @@ def push(url, components=None, component_logs=None):
     import requests
     Log.debug('push() Uploading to url %s' % url)
 
-    content = {}
+    files = {}
+    data = {}
 
     if components:
         comp = components.pop()
         manifest_file = comp._component_file
-        content['manifest'] = open(manifest_file, 'rb')
+        files['manifest'] = open(manifest_file, 'rb')
     if component_logs:
         complog = component_logs.pop()
-        content['exe_hash'] = complog.exe_hash
-    if content:
-        r = requests.post(url, data=content)
+        data['exe_hash'] = complog.exe_hash
+    if files or data:
+        r = requests.post(url, data=data, files=files)
         if r.status_code != requests.codes.ok:
             Log.critical('Error %s occured while upload' % r.status_code)
     if components or component_logs:
