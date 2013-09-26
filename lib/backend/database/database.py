d0cdd4f67df15adaeda88e6caf670be144baad4d
Philipp Offensand <philippoffensand@gmx.de>
2013-09-26 11:57:27 +0200

    Bugfix backend-Datenbank

--- lib/backend/database/database.py
+++ lib/backend/database/database.py
@@ -207,7 +207,7 @@ class Database:
 
     def _query_values(self):
         attributes = self.get_attributes()
-        values = {'adapter': self.__conform__(sqlite3.PrepareProtocol),
+        values = {'adapter': self,
                   }
         for i in attributes:
             if i == 'c_pk':
@@ -245,20 +245,14 @@ class Database:
         for i in attributes:
             if i == 'c_pk':
                 continue
-            sql += """,
-                """ + i
-            sql += " = ':" + i + "'"
+            sql += ", "+ i
+            sql += " = '" + str(self[i]) + "'"
         values['c_pk'] = self.c_pk
-        sql = "UPDATE " + table + """ SET
+        sql = "UPDATE " + table + """ 
+                SET
                 date = (strftime('%s', 'now')),
                 c_id = :c_id,
-                adapter = :adapter""" + sql
-        old_object = self.__class__.get_exactly(self.c_pk, 'c_pk')
-        if self.c_id == old_object.c_id:
-            sql += """
-                WHERE c_id = :c_id """
-        else:
-            sql += """
+                adapter = :adapter """ + sql + """
                 WHERE c_pk = :c_pk """
         return sql, values
 
@@ -294,10 +288,10 @@ class Database:
             self.c_pk = cursor.lastrowid
         except sqlite3.IntegrityError:
             sql, values = self._update_query()
-            Log.debug("Database.save(): %s is old object" %
-                          self.__class__.get_exactly(self.c_pk, 'c_pk'))
+#            Log.debug("Database.save(): %s is old object" %
+#                          self.__class__.get_exactly(self.c_pk, 'c_pk'))
             Log.debug("Database.save(): %s as update query with %s as dict" %
-                          (sql, values))
+                      (sql, values))
             cursor.execute(sql, values)
             Database.db_connection.commit()
         except sqlite3.OperationalError:
